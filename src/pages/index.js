6af3f218ed495ffc32d3c82045908ba7528f5fc4
Josue Diaz <josue.diaz@mindera.com>
2021-01-26 12:11:04 +0000

    route setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,32 @@
 import { lazy } from "react";
 
-import * as urls from "./urls";
+// import * as urls from "./urls";
 
-// const routes = [
-//   {
-//     path: urls.HOMEPAGE_URL,
-//     exact: true,
-//     component: lazy(() => import("./home/Home")),
-//   },
-//   {
-//     path: urls.DISCLOSURE_FORM_URL,
-//     exact: true,
-//     component: lazy(() => import("./disclosure/Disclosure")),
-//   },
-//   {
-//     path: urls.DISCLOSURE_TOKEN_LOGIN_URL,
-//     exact: true,
-//     component: lazy(() => import("./not-found/NotFound")),
-//   },
-//   {
-//     path: urls.DISCLOSURE_DETAILS_URL,
-//     exact: true,
-//     component: lazy(() => import("./not-found/NotFound")),
-//   },
-//   {
-//     path: "*",
-//     component: lazy(() => import("./not-found/NotFound")),
-//   },
-// ];
+const routes = [
+  // {
+  //   path: urls.HOMEPAGE_URL,
+  //   exact: true,
+  //   component: lazy(() => import("./home/Home")),
+  // },
+  //   {
+  //     path: urls.DISCLOSURE_FORM_URL,
+  //     exact: true,
+  //     component: lazy(() => import("./disclosure/Disclosure")),
+  //   },
+  //   {
+  //     path: urls.DISCLOSURE_TOKEN_LOGIN_URL,
+  //     exact: true,
+  //     component: lazy(() => import("./not-found/NotFound")),
+  //   },
+  //   {
+  //     path: urls.DISCLOSURE_DETAILS_URL,
+  //     exact: true,
+  //     component: lazy(() => import("./not-found/NotFound")),
+  //   },
+  {
+    path: "*",
+    component: lazy(() => import("./not-found/NotFound")),
+  },
+];
 
-// export default routes;
+export default routes;
